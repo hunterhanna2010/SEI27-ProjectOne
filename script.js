f52d94840775a8e0cb4b0737907c81788c4dc40b
hunterhann2010 <josh.m.hunter@gmail.com>
2019-10-03 14:22:29 -0700

    This is a submitable project. Except for hint. I will work on that now.

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-//TO DO
-//gather dictionary source: word api or text file? currently using a text file of the 100 most used English words
-//string conversion to array
-//array split in order to process each character individually
-//loop through array
-//array concat based on the player's guess
-//array push into two separate arrays: 1) valid words 2) invalid words
-//loop through both arrays to parse programmatically which one has more
-//apply rounds system: 3 complete rounds for a win
 //randomize which character to give to player when they click hint. 
 //I want to start with longest word left to correctly guess.
 //game settings: initialize game, start game, play game, win game, lose game, restart game
@@ -16,7 +7,7 @@
 const vowelsArray = ['a', 'e', 'i', 'o', 'u'];
 const consonantsArray = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'x', 'y', 'z'];
 var randomLettersArray = []; //limit to 5. this is the total letters from the vowelsArray and the consonantsArray
-var inputArray = []; //we compare inputArray with the randomLettersArray to check if the input matches at any point and in any order.
+//var inputArray = []; //we compare inputArray with the randomLettersArray to check if the input matches at any point and in any order.
 var vowelClicks = 0; //limit to 2
 var consonantClicks = 0; //limit to 3
 var playerScore = 0; //score system updates by player inputs
@@ -29,9 +20,12 @@ var consonantButton = document.getElementById('consonants');
 var hintButton = document.getElementById('hint');
 var submitButton = document.getElementById('submit-word');
 var currentScore = document.getElementById('current-score');
+var resetButton = document.getElementById('reset');
 
 
 //EVENT LISTENERS
+
+
 
 //when the vowel button is clicked, the vowelsArray is randomized and placed in the h2-field for the player to begin
 //after two clicks, the vowel button is disabled and the vowels cannot be added to the randomLettersArray.
@@ -55,6 +49,7 @@ vowelButton.addEventListener('click', function(e) {
         //console.log(letter);
         } else {
             var disableVowels = document.getElementById('vowels').disabled = true;
+            
             //randomLettersArray.pop()
             console.log(`the vowels button no longer works`);
         }
@@ -81,8 +76,28 @@ consonantButton.addEventListener('click', function(e) {
     }
 });
 
-//when the hint button is clicked, the hidden text field will be visible
+resetButton.addEventListener('click', function(e) {
+    console.log('I hate my letters, bring me new ones')
+    var clearVowels = document.getElementById('random-vowel-area').textContent = '';
+    var clearConsonants = document.getElementById('random-consonant-area').textContent = '';
+    var clearInvalidListItem = document.getElementById('invalid-word-list').textContent = '';
+    var clearParaElementTwo = document.getElementById('p-two').textContent = '';
+    var clearParaElementThree = document.getElementById('p-three').textContent = '';
+    var clearParaElementFour = document.getElementById('p-four').textContent = '';
+    var clearParaElementFive = document.getElementById('p-five').textContent = '';
+    var clearPlayerInput = document.getElementById('player-input').textContent = '';
+    var enableVowels = document.getElementById('vowels').disabled = false;
+    console.log(enableVowels)
+    var enableConsonants = document.getElementById('consonants').disabled = false;
+    console.log(enableConsonants)
+    playerScore = 0;
+    currentScore.textContent = `Current Score: ${playerScore}`
+    vowelClicks = 0;
+    consonantClicks = 0;
+})
 
+//when the hint button is clicked, the hidden text field will be visible
+//THIS IS A STRETCH GOAL. I have prioritized adding a reset button and clearing the active fields
 hintButton.addEventListener('click', function(e) {
     console.log(`hint has been clicked`);
     playerScore = playerScore - 10;
@@ -93,70 +108,51 @@ hintButton.addEventListener('click', function(e) {
 
 //the submit button click
 //This block collects the inputvalue (.value)
-//and pushes it into inputArray in order to proceed with game functions.
+//and runs checkDictionary function
 submitButton.addEventListener('click', function(e) {
     console.log(`this is a word button has been clicked`);
     playerInputValue = document.getElementById('player-input').value;
-    // inputArray.push(playerInputValue);
-    // console.log(inputArray)
     console.log(playerInputValue)
-    if (checkLegal(playerInputValue)) {
-        checkDictionary(playerInputValue);
-        checkLetterCount(playerInputValue)
-    };
-    
+    checkMatch(playerInputValue)
 })
 
-//There are five elements to check. 1) compare the arrays, if they do not compare, there is no reason to checkDictionary or count their length
+//There are two main elements to check. 
+//1) does it match as a word in the dictionary?
+//2) how long is it?
+
+//1) if there is no match in the dictionary file, it is invalid.
+//this takes care of any edge cases where someone inputs a period or a comma
+//or uses a contraction apostrophe.
+
+//2) the length of the player input value controls where to put the word
+//in descending order 5 - 2-letter words.
 
 
-//this is the first element to check. This function compares if playerInputs are equivalent to the randomLetterArray only.
-//order does not matter, only if there is an instance where the playerInput compares true to the randomLetterArray.
-//in this block, I do not check if it is a word, that will be the checkDictionary function.
-//if true, it is valid to the randomLettersArray and can go in the correct area
-//else, it is false, therefore invalid, because the inputArray did not match the randomLettersArray
-function checkLegal(playerInputValue) {
-    //check if the length of the guess is longer than the random letters array
-//     if (playerInputValue > randomLettersArray.length) {
-//         //iterate over playerInput
-//         for (let i = 0; i < inputArray.length; i++) {
-//             var legal = randomLetterArray.includes(inputArray[i])
-//             console.log(legal)
-//         }
-//     }
-// }
-    
-    
-    // check for legality
-    // see if what the person imputed has any characters that are not in your random letters array
-    // return value will be a boolean
-
-    // iterate through MOST RECENT guess.
-    // for looping through the recentGuess.length
-    // if random letters array includes recentGuess[i]
+//this is the first element to check. And this is the bulk of the game logic. This one function negates the need for three separate 
+//verifications. Now, I don't need to see if the individual characters match, because I am seeing if the word is in the file at any point.
+//This is the priority for my dictionary file as is, as well as any future implementations. 
+function checkMatch(playerInputValue) {
     var listItem = document.createElement('li');
-    //for (let i = 0; i < inputArray.length; i++) {
-        var match = dictionary.includes(playerInputValue);
-        console.log("Input array at i is: " + playerInputValue)
-        //console.log("🍑"+i)
+    var match = dictionary.includes(playerInputValue);
         console.log(match)
+        //if NOT a match with word-dictionary, then reject it as invalid. Deduct two points.
         if (!match) {
             console.log('your input was NOT in the array')
             console.log(playerInputValue)
             var notInTheArray = document.getElementById('invalid-word-list')
-            playerScore = playerScore - 2;
             listItem.textContent = playerInputValue;
             notInTheArray.appendChild(listItem);
             console.log(notInTheArray)
+            playerScore = playerScore - 2;
+            currentScore.textContent = `Current Score: ${playerScore}`
         } else {
+            //It IS a match, therefore now valid and sent to the correct div container based on the value.length
             console.log('your input was in the array')
             switch(playerInputValue.length) {
                 case 5:
                     console.log(`this is number 5`);
-                    var five = document.getElementById('five');
-                    var paraElementFive = document.createElement('P');
-                    paraElementFive.textContent = playerInputValue;
-                    five.append(playerInputValue);
+                    var five = document.getElementById('p-five');
+                    five.textContent = playerInputValue;
                     playerScore = playerScore + 5;
                     currentScore.textContent = `Current Score: ${playerScore}`;
                     console.log(paraElementFive)
@@ -164,30 +160,24 @@ function checkLegal(playerInputValue) {
                     break;
                 case 4:
                     console.log(`this is number 4`)
-                    var four = document.getElementById('four');
-                    var paraElementFour = document.createElement('P');
-                    paraElementFour.textContent = playerInputValue;
-                    four.append(playerInputValue);
+                    var four = document.getElementById('p-four');
+                    four.textContent = playerInputValue;
                     playerScore = playerScore + 4;
                     currentScore.textContent = `Current Score: ${playerScore}`
                     console.log(four)
                     break;
                 case 3:
                     console.log(`this is number 3`)
-                    var three = document.getElementById('three');
-                    var paraElementThree = document.createElement('P');
-                    paraElementThree.textContent = playerInputValue;
-                    three.append(playerInputValue);
+                    var three = document.getElementById('p-three');
+                    three.textContent = playerInputValue;
                     playerScore = playerScore + 3;
                     currentScore.textContent = `Current Score: ${playerScore}`
                     console.log(three)
                     break;
                 case 2:
                     console.log(`this is number 2`)
-                    var two = document.getElementById('two');
-                    var paraElementTwo = document.createElement('P');
-                    paraElementTwo.textContent = playerInputValue;
-                    two.append(playerInputValue);
+                    var two = document.getElementById('p-two');
+                    two.textContent = playerInputValue;
                     playerScore = playerScore + 2
                     currentScore.textContent = `Current Score: ${playerScore}`
                     console.log(two)
@@ -205,40 +195,8 @@ function checkLegal(playerInputValue) {
 }
 
 
-//this is the second element to check. This function tests whether the input includes a word from the dictionary file
-//if it is true, it is a word in the dictionary, at it to a new array 
-//else, it is not in the dictionary, and is false, therefore invalid.
-function checkDictionary(playerInputValue) {
-    if (dictionary[playerInputValue]) {
-        return true;
-        // if (dictionary.hasOwnProperty(word)) {
-        //     console.log('this is word (' + 
-        //     word + ') for sure. Value: ' + dictionary[word]);
-        //}
-    } 
-    return false;
-}
-//         else {
-//           console.log(word); // toString or something else
-//         }
-//       }
-    // for (let [word, value] of Object.entries(dictionary)) {
-    //     let value = console.log((`${word}: ${value}`));
-    //     console.log(value)
-    // }
-//}
-    //checkDictionary();
-    
-//This is the third element to check. the switch statement tells the valid word where to go and how many points to give to the player.
-//The switch is based on letter count. If the input is more than five characters - it is invalid, and will be added to the invalid list.
-//this switch statement is working in its current implementation.
-function checkLetterCount(playerInputValue) {
-    
-}
-    
 
-
-
+    
 
 //this fourth element is to iterate over the randomLettersArray and see how many word combinations are present
 //leftOver words must be english words defined in the word-dictionary file, exclude one letter words and the articles 'a', 'an', and 'the'
